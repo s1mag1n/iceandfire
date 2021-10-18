543481d49f4a00ed2791d67adf3be3e3b71e6d93
s1mag1n <57657053+s1mag1n@users.noreply.github.com>
2021-10-18 11:08:45 +0300

    Add files via upload

---
+++ Ice_And_Fire-master/src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spinner-wrevurafrdm"><div class="ldio-4lyksrrbqcl">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div>
+    )
+}
+
+export default Spinner;
